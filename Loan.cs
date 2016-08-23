323936462238e2b553ffeee9e23f2871059a8c69
HoOlivier <olivier@hogne.com>
2016-08-23 16:22:09 +0200

    Revert "Revert "just a test""
    This reverts commit bc520f352c73b5a93fc43eb525a61d521a9c9cd1.

---
+++ Loan.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageLoan
+{
+    class Loan
+    {
+        // Loan parameters.
+        public double capital;
+        public double duration;
+        public double rate;
+        public double loan;
+
+        // Loan constructor.
+        public Loan(double cap, double dur, double rat, double loa)
+        {
+            capital = cap;
+            duration = dur;
+            rate = rat;
+            loan = loa;
+        }
+
+        // Solving for capital.
+        public void solveCapital()
+        {
+            capital = 12 * duration * loan;
+            double range = capital/2;
+            double res = loanResidue();
+                        
+            while (!loanIsSolved())
+            {
+                System.Diagnostics.Debug.WriteLine("Test: {0} +- {1}", capital,range);
+                if (res>=0.01)
+                {
+                    capital -= range;
+                    range /= 2;
+                    res = loanResidue();
+                }
+                else
+                {
+                    res = capital;
+                    capital += range;
+                    range /= 2;
+                    res = loanResidue();
+                }
+            }
+
+        }
+
+        // Solving for monthly loan.
+        public void solveLoan()
+        {
+            var solvedLoa = new double();
+            // solving
+            loan = solvedLoa;
+        }
+
+        // Solving for needed annual rate.
+        public void solveRate()
+        {
+            var solvedRat = new double();
+            // solving
+            rate = solvedRat;
+        }
+
+
+        public double loanResidue()
+        {
+            double residue = capital;
+            var interests = new double();
+            double monthlyRate = Math.Pow(rate+1, Math.Pow(12, -1)) - 1;
+            for (int i = 0; i < 12*duration; i++)
+            {
+                interests = monthlyRate * residue;
+                residue += interests - loan;
+            }
+
+            return residue;
+        }
+
+        public bool loanIsSolved()
+        {
+            return ((loanResidue() < 0.01) & (loanResidue() > -0.01));
+        }
+    }
+}
---
+++ SimulationPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MortgageLoan
+{
+    public class SimulationPage : ContentPage
+    {
+        public SimulationPage()
+        {
+            var menu = new Label { Text = "I want to simulate..." };
+
+            var button1 = new Button { Text = "...how much I can borrow" };
+            button1.Clicked += onButton1Clicked;
+
+            var button2 = new Button { Text = "...how much I will pay monthly" };
+            button2.Clicked += onButton2Clicked;
+
+            var button3 = new Button { Text = "...which rate I should aim for" };
+            button3.Clicked += onButton3Clicked;
+
+            Title = "Main menu";
+            Content = new StackLayout
+            {
+                Children = { menu, button1, button2, button3 }
+            };
+        }
+
+        async void onButton1Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CapitalSim());
+        }
+
+        async void onButton2Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoanSim());
+        }
+
+        async void onButton3Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RateSim());
+        }
+    }
+}
